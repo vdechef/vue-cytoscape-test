7be733befec44db7cff49c29913278ec6e863b9d
Robert Carcassés Quevedo <carcasses@gmail.com>
2018-10-26 16:51:55 +0100

    Minimalist example working, production built distribution also working.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="holder">
+    <cytoscape :config="config" :preConfig="preConfig" :afterCreated="afterCreated"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable */
+const config = {
+  elements: [
+    { // node a
+      data: { id: 'a' }
+    }, { // node b
+      data: { id: 'b' }
+    }, { // edge ab
+      data: { id: 'ab', source: 'a', target: 'b' }
+    }
+  ],
+  style: [
+    {
+      selector: 'node',
+      style: {
+        'background-color': '#666',
+        'label': 'data(id)'
+      }
+    }, {
+      selector: 'edge',
+      style: {
+        'width': 3,
+        'line-color': '#ccc',
+        'target-arrow-color': '#ccc',
+        'target-arrow-shape': 'triangle'
+      }
+    }
+  ],
+  layout: {
+    name: 'grid',
+    rows: 1
+  }
+}
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'App',
+  data () {
+    return {
+      config,
+      i: 1
+    }
+  },
+  methods: {
+    preConfig (cytoscape) {
+      console.log('calling pre-config')
+	  // let's register a tap event, for instance
+      const that = this
+      this.$cytoscape.reset()
+      this.$cytoscape.instance.then(cy => {
+        cy.on('tap', event => {
+          console.log('tapped by', that.i, 'time')
+          that.i++
+        })
+      })
+    },
+    afterCreated (cy) {
+      console.log('after created')
+    }
   }
 }
 </script>
 
 <style>
+#holder {
+  width: 100%;
+  height: 400px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueCytoscape from 'vue-cytoscape'
+import 'vue-cytoscape/dist/vue-cytoscape.css'
+
+Vue.use(VueCytoscape)
 
 Vue.config.productionTip = false
 
